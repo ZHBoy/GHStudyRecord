f6cd1a3f6568a0b7651534e2eb8fae015ccea8ff
shabake <45329453@qq.com>
2019-01-25 15:32:03 +0800

    creat alignment rect

---
+++ OC/Demo/alignment rect/alignment rect/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  alignment rect
+//
+//  Created by zhaozhiwei on 2019/1/25.
+//  Copyright © 2019年 GHome. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ OC/Demo/alignment rect/alignment rect/ViewController.m
@@ -0,0 +1,24 @@
+//
+//  ViewController.m
+//  alignment rect
+//
+//  Created by zhaozhiwei on 2019/1/25.
+//  Copyright © 2019年 GHome. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController () {
+    NSString *_test;
+}
+@property (nonatomic , strong) id object;
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+
+@end
