4e95d805eaadae371c4d7d05b0a02943c858907b
shabake <45329453@qq.com>
2019-02-20 17:46:59 +0800

    创建项目

---
+++ OC/App/GHGitHub/Demo/GHGitHubDemo/GHGitHub-OC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GHGitHub-OC
+//
+//  Created by zhaozhiwei on 2019/2/20.
+//  Copyright © 2019年 GHome. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
