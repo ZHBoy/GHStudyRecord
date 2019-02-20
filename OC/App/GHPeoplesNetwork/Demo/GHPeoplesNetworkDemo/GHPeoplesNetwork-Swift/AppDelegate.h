46fcdee639ec9c29b6e1f4e28081ea6d776ccae5
shabake <45329453@qq.com>
2019-02-20 17:54:06 +0800

    修改目录

---
+++ OC/App/GHPeoplesNetwork/Demo/GHPeoplesNetworkDemo/GHPeoplesNetwork-Swift/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GHPeoplesNetwork-Swift
+//
+//  Created by zhaozhiwei on 2019/2/20.
+//  Copyright © 2019年 GHome. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OC/App/GHPeoplesNetwork/Demo/GHPeoplesNetworkDemo/GHPeoplesNetwork-Swift/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GHPeoplesNetwork-Swift
+//
+//  Created by zhaozhiwei on 2019/2/20.
+//  Copyright © 2019年 GHome. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
